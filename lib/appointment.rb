cd1f0a815a86d336ffd21fd6b48689b4eddfd3e7
DAZUN XING <dazun.xing@gmail.com>
2020-05-23 06:13:53 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,7 +2,14 @@ class Appointment
   @@all =[]
   attr_accessor :date, :patient, :doctor
   def initialize(date,patient,doctor)
-    
+    @date =date
+    @patient =patient
+    @doctor =doctor
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
   end
   
 end

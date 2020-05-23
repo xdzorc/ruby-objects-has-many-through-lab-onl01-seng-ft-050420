427805ccfdbdef5108bcca427fbfd69d8ba3d995
DAZUN XING <dazun.xing@gmail.com>
2020-05-23 05:53:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,14 @@
 class Song 
   @@all =[]
-  attr_accessor :name 
+  attr_accessor :name, :artist ,:genre
   def initialize(name,artist,genre)
     @@all << self 
     @name =name
+    @artist =artist
+    @genre =genre
+  end
+  
+  def self.all 
+    @@all
   end
 end
